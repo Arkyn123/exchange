796f70383d6139bacff34c5723b6c27ecabb5540
Arkyn <arkyn00@mail.ru>
2024-08-09 14:56:09 +0500

    add contractAddr

--- src/eth-listener/eth-listener.service.ts
+++ src/eth-listener/eth-listener.service.ts
@@ -6,7 +6,7 @@ import { abi } from './abi';
 @Injectable()
 export class EthListenerService implements OnModuleInit {
     private web3: Web3
-    private contractAddr: string
+    private contractAddr: string = '0xa26761BC436aC2b280c092D1D6FCa56e9728cbb8'
     private contract: any
 
     constructor(
@@ -16,6 +16,6 @@ export class EthListenerService implements OnModuleInit {
     async onModuleInit() {
         this.orderService.test()
         this.web3 = new Web3(new Web3.providers.WebsocketProvider('wss://arbitrum-mainnet.io/ws/v3/398f8a9407bd45189b02a5d27311a8bb'));
-        this.contract = new this.web3.eth.Contract(abi, contractAddress);
+        this.contract = new this.web3.eth.Contract(abi, this.contractAddr);
     }
 }
